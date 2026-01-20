afedd0a52ec34d0fa26bf17310c9ffc7f6e14ba3
Muhammad Ahmed <gaming.indoor.71@gmail.com>
2026-01-20 23:00:06 +0500

    error fixed

--- All-tasks/src/pages/Task1.jsx
+++ All-tasks/src/pages/Task1.jsx
@@ -1,4 +1,4 @@
-import Navigation from "../components/navigation"
+import Navigation from "../components/Navigation"
 import StudentCard from "../components/StudentCard"
 
 const Task1 = () => {
--- All-tasks/src/pages/Task4.jsx
+++ All-tasks/src/pages/Task4.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navigation from "../components/navigation";
+import Navigation from "../components/Navigation";
 
 const Task4 = () => {
 
